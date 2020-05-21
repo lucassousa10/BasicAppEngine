e2fabbd17e0fb4279cc425a7be5fc823747ed77d
god-lucas@hotmaill.com <Lucas5411459>
2020-05-21 19:04:15 -0300

    code format

--- src/com/engine/util/ElapsedTimeChecker.java
+++ src/com/engine/util/ElapsedTimeChecker.java
@@ -6,10 +6,7 @@ public class ElapsedTimeChecker {
 
     public static void check(Action action, int times, boolean useNanos) {
         long begin = useNanos ? System.nanoTime() : System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            action.perform();
-        }
-
+        for (int i = 0; i < times; i++) action.perform();
         long end = (useNanos ? System.nanoTime() : System.currentTimeMillis()) - begin;
         System.out.println("Spent " + end + (useNanos ? "ns" : "ms") + " to perform " + times + " times.");
     }
